82e0407d6f157080acbbfc8117d7eb3aea65835a
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-04-19 14:21:28 +0500

    6

--- if06.py
+++ if06.py
@@ -13,4 +13,22 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    return
+    z=0
+    if a<0:
+        z=z+1
+    if b<0:
+        z=z+1
+    if c<0:
+        z=z+1
+    s=0
+    if a>0:
+        s=s+1
+    if b>0:
+        s=s+1
+    if c>0:
+        s=s+1
+    if z>s: 
+        return "there are a lot of negative numbers"
+    if z<s:
+        return "there are a lot of positive numbers"
+print(main(2,3,-5))
